488202b8cadb073a63a37ce5f8dab4992e01f671
Thiago Ururay <taraujo@id.uff.br>
2017-03-29 03:18:12 -0300

    example slide code

--- mpi_studies/first/first.c
+++ mpi_studies/first/first.c
@@ -3,14 +3,23 @@
 #include <mpi.h>
 
 int main(int argc, char** argv) {
-  int my_rank, processes_amount, origin, destiny, tag=0;
-  char msg[100];
+  int my_rank, processes_amount, origin_process, destiny_process, tag=0;
+  char message[100];
   MPI_Status status;
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &processes_amount);
 
-  printf("Eu sou o processo número %i de %i no grupo World\n", my_rank, processes_amount);
+  if(my_rank != 0) {
+    sprintf(message, "Processo %d está vivo!", my_rank);
+    destiny_process = 0;
+    MPI_Send(message, strlen(message)+1, MPI_CHAR, destiny_process, tag, MPI_COMM_WORLD);
+  } else {
+    for (origin_process = 1; origin_process < processes_amount; origin_process++) {
+      MPI_Recv(message, 100, MPI_CHAR, origin_process, tag, MPI_COMM_WORLD, &status);
+      printf("%s\n", message);
+    }
+  }
 
   MPI_Finalize();
 }
