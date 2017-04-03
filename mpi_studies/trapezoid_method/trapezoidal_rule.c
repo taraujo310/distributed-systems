299d185a40eda9a0e7688ef2f295eea0c6636167
Thiago Ururay <taraujo@id.uff.br>
2017-04-02 23:32:00 -0300

    trapezoidal rule with remaining rate on trapezoids and processes amount treatment

--- mpi_studies/trapezoid_method/trapezoidal_rule.c
+++ mpi_studies/trapezoid_method/trapezoidal_rule.c
@@ -11,16 +11,17 @@ int main(int argc, char** argv) {
 
   // Trapezoid Method specifc variables
   float a=0.0, b=1.0, height, interval_start, interval_end, integral, total;
-  int intervals=1024, process_intervals;
+  int intervals=1024, process_intervals, remaining_process_intervals;
 
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &processes_amount);
-  if(rank == 0) start = MPI_Wtime();
+  if(rank == 0) start=MPI_Wtime(); /*start timer*/
 
   // Common calculation
   height = (b-a)/intervals;
   process_intervals = intervals/processes_amount;
+  remaining_process_intervals = intervals%processes_amount;
 
   // Processe specific calculation
   interval_start = a + rank * process_intervals * height;
@@ -30,9 +31,20 @@ int main(int argc, char** argv) {
 
   MPI_Reduce(&integral, &total, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
 
-  if(rank == 0) printf(">>> Resultado: %f\n", total);
+  MPI_Barrier(MPI_COMM_WORLD);
+  if(rank == 0) {
+    if (remaining_process_intervals != 0) {
+      interval_start = a + processes_amount * process_intervals * height;
+      interval_end = b;
 
-  finish = MPI_Wtime();
+      printf("Process 0 calculating the remaining interval [%f, %f]\n", interval_start, interval_end);
+
+      total += compute_interval(interval_start, interval_end, remaining_process_intervals, height);
+    }
+    printf(">>> Resultado: %f\n", total);
+  }
+
+  finish=MPI_Wtime();
 
   if(rank == 0)
     printf("Tempo passado durante o processamento: %f\n", finish-start);
