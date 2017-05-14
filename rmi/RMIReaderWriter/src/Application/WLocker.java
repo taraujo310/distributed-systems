f832306c3ab74d1bcd4422e6a5fcc9c077b1aa31
Thiago Ururay <taraujo@id.uff.br>
2017-05-14 16:33:26 -0300

    returning with readersLockerMutex

--- rmi/RMIReaderWriter/src/Application/WLocker.java
+++ rmi/RMIReaderWriter/src/Application/WLocker.java
@@ -7,9 +7,10 @@ public class WLocker implements RWLockable {
 	int readersCounter = 0, writersCounter = 0;
 	Semaphore readersCounterMutex = new Semaphore(1), // protects readersCounter
 			writersCounterMutex = new Semaphore(1), // protects writersCounter
+      readersLockerMutex = new Semaphore(1), // Protects readers locker
 			closeToWriters = new Semaphore(1),
 			closeToReaders = new Semaphore(1);
-	
+
 	public WLocker(String name) {
 		r = new Resource(name);
 	}
@@ -21,33 +22,35 @@ public class WLocker implements RWLockable {
 
 	@Override
 	public void requestRead() throws InterruptedException {
+    readersLockerMutex.acquire();
 		closeToReaders.acquire();
 		readersCounterMutex.acquire();
-		
-		if(++readersCounter == 1) 
+
+		if(++readersCounter == 1)
 			closeToWriters.acquire();
-		
+
 		readersCounterMutex.release();
 		closeToReaders.release();
+    readersLockerMutex.release();
 	}
 
 	@Override
 	public void releaseRead() throws InterruptedException {
 		readersCounterMutex.acquire();
-		
-		if(--readersCounter == 0) 
+
+		if(--readersCounter == 0)
 			closeToWriters.release();
-		
+
 		readersCounterMutex.release();
 	}
 
 	@Override
 	public void requestWriting() throws InterruptedException {
 		writersCounterMutex.acquire();
-		
+
 		if (++writersCounter == 1)
 			closeToReaders.acquire(); // impede a entrada de leitores na região crítica
-		
+
 		writersCounterMutex.release();
 		closeToWriters.acquire(); // impede escritores simultâneos na região crítica
 	}
@@ -56,10 +59,10 @@ public class WLocker implements RWLockable {
 	public void releaseWriting() throws InterruptedException {
 		closeToWriters.release();
 		writersCounterMutex.acquire();
-		
+
 		if (--writersCounter == 0)
 			closeToReaders.release();
-		
+
 		writersCounterMutex.release();
 	}
 
